6dadf9e5324582d68701ebcd3f7013515a76e5e7
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2022-12-28 17:42:40 +0200

    hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,14 +6,16 @@ const textarea = document.querySelector('textarea');
 
 const formData = localStorage.getItem('feedback-form-state');
 const parsedFormData = JSON.parse(formData);
-input.value = parsedFormData.email ? parsedFormData.email : '';
-textarea.value = parsedFormData.message ? parsedFormData.message : '';
+if (parsedFormData) {
+  input.value = parsedFormData.email;
+  textarea.value = parsedFormData.message;
+}
 
 form.addEventListener('input', throttle(handleInput, 500));
 form.addEventListener('submit', handleSubmit);
 
 function handleInput(event) {
-  if(event.currentTarget.elements){
+  if (event.currentTarget.elements) {
     const { email, message } = event.currentTarget.elements;
     localStorage.setItem('feedback-form-state', JSON.stringify({ email: email.value, message: message.value }));
   }
